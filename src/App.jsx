faa609a7dc0309aca288922ecf28668befa27817
Shawkat-Sujon <sujoniutswe@gmail.com>
2023-06-03 00:24:42 +0600

    Firebase db connection

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,38 @@
 
+import { useEffect, useState } from 'react';
 import './App.css';
 import { Auth } from './components/auth';
-
+import { db } from './config/firebase';
+import { getDocs, collection } from 'firebase/firestore';
 
 function App() {
+  const [movieList, setMovieList] = useState([]);
+  const moviesCollectionRef = collection(db, "movies")
+  useEffect(() => {
+    const getMovieList = async () => {
+      try{
+        const data = await getDocs(moviesCollectionRef)
+        const filteredData = data.docs.map((doc) => ({...doc.data(), id: doc.id,}))
+        setMovieList(filteredData)
+      } catch (err) {
+        console.error(err)
+      }
+      
+    }
+    getMovieList();
+  }, [])
+
   return (
     <div className="App">
       <Auth />
+      <div>
+        {movieList.map((movie) => (
+          <div>
+            <h1 style={{color: movie.receivedAnOscar? "green": "red" }}>{movie.title}</h1>
+            <p>Date: {movie.releaseDate}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
