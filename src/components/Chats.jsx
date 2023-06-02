df5c730ee1487f77527a619dcd392085916d3d09
Shawkat-Sujon <sujoniutswe@gmail.com>
2023-06-02 13:16:02 +0600

    Sidebar and Chats done

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -2,6 +2,31 @@ import React from 'react'
 
 export const Chats = () => {
   return (
-    <div>Chats</div>
+    <div className='chats'>
+      <div className="userChat">
+            <img src="https://images.pexels.com/photos/17045026/pexels-photo-17045026/free-photo-of-fashion-people-woman-relaxation.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load" alt="" />
+            <div className="userChatInfo">
+                <span>Shawkat</span>
+            </div>
+        </div>
+        <div className="userChat">
+            <img src="https://images.pexels.com/photos/17045026/pexels-photo-17045026/free-photo-of-fashion-people-woman-relaxation.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load" alt="" />
+            <div className="userChatInfo">
+                <span>Shawkat</span>
+            </div>
+        </div>
+        <div className="userChat">
+            <img src="https://images.pexels.com/photos/17045026/pexels-photo-17045026/free-photo-of-fashion-people-woman-relaxation.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load" alt="" />
+            <div className="userChatInfo">
+                <span>Shawkat</span>
+            </div>
+        </div>
+        <div className="userChat">
+            <img src="https://images.pexels.com/photos/17045026/pexels-photo-17045026/free-photo-of-fashion-people-woman-relaxation.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load" alt="" />
+            <div className="userChatInfo">
+                <span>Shawkat</span>
+            </div>
+        </div>
+    </div>
   )
 }
